a1d231b7e2c8586f32a77513c3b6952e3fa71781
jun-mac-pro <number9286@gmail.com>
2026-09-20 04:46:28 +0900

    fix(magazine): 제주 지역 코드 이름 매핑 추가

--- src/main/java/com/mulmeong/domain/magazine/service/MagazineService.java
+++ src/main/java/com/mulmeong/domain/magazine/service/MagazineService.java
@@ -148,7 +148,8 @@ public class MagazineService {
                         Map.entry("28", "인천"), Map.entry("29", "광주"), Map.entry("30", "대전"),
                         Map.entry("31", "울산"), Map.entry("43", "충북"), Map.entry("44", "충남"),
                         Map.entry("46", "전남"), Map.entry("47", "경북"), Map.entry("48", "경남"),
-                        Map.entry("51", "강원"), Map.entry("52", "전북"), Map.entry("41", "경기"))
+                        Map.entry("50", "제주"), Map.entry("51", "강원"), Map.entry("52", "전북"),
+                        Map.entry("41", "경기"))
                 .getOrDefault(code, code);
     }
 }
