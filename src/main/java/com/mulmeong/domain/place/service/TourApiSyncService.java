2be4a4fd682ccf2fa85305af908751cc592918c1
jun-mac-pro <number9286@gmail.com>
2026-09-21 02:33:20 +0900

    fix(place): TourApiSyncService 예외 import 누락 수정

--- src/main/java/com/mulmeong/domain/place/service/TourApiSyncService.java
+++ src/main/java/com/mulmeong/domain/place/service/TourApiSyncService.java
@@ -3,6 +3,7 @@ package com.mulmeong.domain.place.service;
 import com.mulmeong.domain.place.entity.Place;
 import com.mulmeong.domain.place.entity.PlaceType;
 import com.mulmeong.domain.place.repository.PlaceRepository;
+import com.mulmeong.global.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
